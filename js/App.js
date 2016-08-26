b947051246dda8e29deefeeb10eb800c26c62c38
syzer3@gmail.com <syzer3@gmail.com>
2016-08-26 09:57:55 +0200

    Routes

--- js/App.js
+++ js/App.js
@@ -2,12 +2,9 @@ import React from 'react'
 import {render} from 'react-dom'
 import {browserHistory, Router, Route, IndexRoute} from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
-
-import App from '../components/App'
-import Home from '../components/Home'
-import About from '../components/About'
-import Features from '../components/Features'
+import MyButton from './Button';
+import Home from './Route/Home'
+import About from './Route/About'
 
 render(
     <Router history={browserHistory}>
@@ -15,21 +12,10 @@ render(
             <IndexRoute component={Home}/>
             <Route path='about' component={About}/>
             <Route path='features' component={Features}/>
+            <MuiThemeProvider>
+                <MyButton />
+            </MuiThemeProvider>
         </Route>
     </Router>,
     document.getElementById('app')
 )
-
-
-
-
-const App = () => (
-    <MuiThemeProvider>
-        <MyAwesomeReactComponent />
-    </MuiThemeProvider>
-);
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
